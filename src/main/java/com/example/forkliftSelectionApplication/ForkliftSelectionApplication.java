f7f743e25f5d09ccc0db876b266d5a1547e4977b
Kerim Kukuljac <kerimkukuljac@gmail.com>
2023-07-17 02:56:43 +0200

    project initalization connect to database

---
+++ src/main/java/com/example/forkliftSelectionApplication/ForkliftSelectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.forkliftSelectionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkliftSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkliftSelectionApplication.class, args);
+		System.out.println("project initialization");
+	}
+ 
+}
---
+++ src/test/java/com/example/forkliftSelectionApplication/ForkliftSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forkliftSelectionApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkliftSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
